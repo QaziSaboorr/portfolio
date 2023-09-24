d10a8937bb64c34e65bd5344fe85309664de7896
QaziSaboorr <qazi.ali@ucalgary.ca>
2023-09-24 16:53:40 -0600

    linkedin

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -49,7 +49,7 @@ const Contact = () => {
           Linkedin:{" "}
         </span>
         <a
-          href="https://www.linkedin.com/public-profile/settings?trk=d_flagship3_profile_self_view_public_profile"
+          href="https://www.linkedin.com/in/qazi-saboor-ali-231aa4292/"
           target="_blank"
           className="text-yellow-600 text-decoration-line: underline font-style: italic  hover:text-red-400 "
         >
